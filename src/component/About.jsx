a8f2cdf48308f31d6a7c6679314e2a011dc2bbec
Yoan Gvs <yoangovaerts@gmail.com>
2024-03-29 16:38:42 +0100

    Header + Hero html section

---
+++ src/component/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ppImg from '../assets/img/hero-img.webp'
+
+const About = () => {
+  return <div className="About-container"></div>
+}
+
+export default About
---
+++ src/component/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+const Header = () => {
+  return (
+    <div>
+      <h3 className="logo">Yoan.dev</h3>
+      <Nav />
+    </div>
+  )
+}
+
+export default Header
